123a9c71cc58d77485516b5670ed409343621041
nitchadew <deathoption_2@hotmail.com>
2016-04-05 00:54:04 +0700

    *01*Window
    หน้าต่างสีดำครับ

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("TEST");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 600);
+
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setVisible(true);
+		
+	
+		
+		
+	}
+}
